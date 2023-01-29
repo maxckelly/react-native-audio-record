ae81a30d316753e5dd77a7aa91e19f426897aa08
Meher-MihikaMacAir <hmeherranjan@gmail.com>
2023-01-29 22:30:31 +0530

    updating some code based on react native track player

--- ios/RNAudioRecord/RNAudioRecord.m
+++ ios/RNAudioRecord/RNAudioRecord.m
@@ -1,19 +1,14 @@
-//
-//  RNAudioRecordBridge.m
-//  RNAudioRecord
-//
-//  Created by James Kemp on 17/5/2022.
-//
-
-#import "React/RCTEventEmitter.h"
-#import "React/RCTBridgeModule.h"
+#import "RNAudioRecord-Bridging-Header.h"
+#import <Foundation/Foundation.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTBridgeModule.h>
 
 
-@interface
-    RCT_EXTERN_MODULE(RNAudioRecord, RCTEventEmitter)
-    RCT_EXTERN_METHOD(initialise:(NSDictionary *) options)
-    RCT_EXTERN_METHOD(start:(NSDictionary *) playbackOptions)
-    RCT_EXTERN_METHOD(stop:
-                        (RCTPromiseResolveBlock)resolve
-                        rejecter:(RCTPromiseRejectBlock)reject)
+@interface RCT_EXTERN_MODULE(RNAudioRecord, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(initialise:(NSDictionary *) options)
+RCT_EXTERN_METHOD(start:(NSDictionary *) playbackOptions)
+RCT_EXTERN_METHOD(stop:
+                    (RCTPromiseResolveBlock)resolve
+                    rejecter:(RCTPromiseRejectBlock)reject)
 @end
